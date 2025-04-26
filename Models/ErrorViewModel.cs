0af96b851b6a932d48183f0bc248a3dd65992f8d
P3dito <palominozaratep@gmail.com>
2025-04-25 20:28:58 -0500

    Inicializando proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Parcial_2025_I.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
